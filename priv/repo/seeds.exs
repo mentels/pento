edfdda784d422b600bf06c9e15b8ed232b8f2f04
Szymon Mentel <szymon@fly.io>
2024-08-07 08:05:32 +0200

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,33 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Pento.Catalog
+
+products = [
+  %{
+    name: "Chess",
+    description:
+      "A two-player strategy board game played on a checkered board with 64 squares arranged in an 8×8 grid.",
+    sku: 5_678_910,
+    unit_price: 10.00
+  },
+  %{
+    name: "Monopoly",
+    description:
+      "A board game that is played by two to eight players. It is a game of luck, strategy, and critical thinking.",
+    sku: 1_234_567,
+    unit_price: 20.00
+  },
+  %{
+    name: "Risk",
+    description:
+      "A strategy board game of diplomacy, conflict and conquest for two to six players.",
+    sku: 9_876_543,
+    unit_price: 15.00
+  }
+]
+
+Enum.each(products, fn product ->
+  Catalog.create_product(product)
+end)
