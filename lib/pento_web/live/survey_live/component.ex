0cf33cab80c2bf820781eb8d5cc65a32cd0a4999
Szymon Mentel <mentel.szymon@gmail.com>
2025-01-07 15:05:34 +0100

    Add survey_live* with some debug code

---
+++ lib/pento_web/live/survey_live/component.ex
@@ -0,0 +1,24 @@
+defmodule PentoWeb.SurveyLive.Component do
+  use Phoenix.Component
+
+  attr :content, :string, required: true
+  slot :inner_block, required: true
+
+  def hero(assigns) do
+    ~H"""
+    <h1 class="font-heavy ext-3xl">
+      <%= @content %>
+    </h1>
+    <h3>
+      <%= render_slot(@inner_block) %>
+    </h3>
+    <pre>
+      <%= inspect(assigns, pretty: true) %>
+      <% %{ inner_block: [%{inner_block: block_fn}] } = assigns %>
+      <%= inspect(block_fn.(assigns.__changed__, assigns), pretty: true) %>
+      <% %{dynamic: dynamic} = block_fn.(assigns.__changed__, assigns) %>
+      dynamic: <%= inspect(dynamic.(assigns), pretty: true) %>
+    </pre>
+    """
+  end
+end
