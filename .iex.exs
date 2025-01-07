6e30a25ce974bfdb1359b4df65966a861a6fe270
Szymon Mentel <mentel.szymon@gmail.com>
2025-01-07 15:03:53 +0100

    Add user creation to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,4 @@
+import Ecto.Query
 alias Pento.Repo
 
 alias Pento.{
@@ -16,7 +17,16 @@ Repo.delete_all(Catalog.Product)
 Repo.delete_all(User)
 
 {:ok, user} =
-  Accounts.register_user(%{email: "tom@dom.qu", password: "123456789abc", username: "tom"})
+  Accounts.register_user(%{
+    email: "mentel.szymon@gmail.com",
+    password: "ahw1WZA4knu*kez@bkz",
+    username: "mentels"
+  })
+
+Repo.update_all(
+  from(u in User, where: u.id == ^user.id),
+  set: [confirmed_at: NaiveDateTime.utc_now(:second)]
+)
 
 {:ok, demographic} =
   Survey.create_demographic(%{user_id: user.id, gender: "male", year_of_birth: 1988})
